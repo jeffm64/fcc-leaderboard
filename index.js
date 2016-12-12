bb18cbef9579bd79b7620f0434ddf42b891a9549
Jeff <jeff_m_england@yahoo.com>
2016-12-12 18:53:13 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var app = express();
+var path = require("path");
+
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+"/index.html"));
+});
+
+app.listen(process.env.PORT || 8080, function () {
+  console.log("App listening on port 8080!");
+});
